e9b72f748e80db6baf9bee9fed26114520a50a3a
romdoniahmad185-cmyk <romdoniahmad185@gmail.com>
2026-08-02 21:39:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ app = FastAPI(
     description="API AI Chatbot WEBKITA",
     version="1.0.0"
 )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # ===============================
 # Inisialisasi Chatbot
 # ===============================
