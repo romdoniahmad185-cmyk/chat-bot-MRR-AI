512e873a5f381c63a812829927c8cbac7701ba9f
romdoniahmad185-cmyk <romdoniahmad185@gmail.com>
2026-08-03 15:27:59 +0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -295,6 +295,16 @@ class Chatbot:
         }
 
 
+# ==============================
+# Cari Percakapan
+# ==============================
+
+hasil_percakapan = self.cari_percakapan(
+   pertanyaan
+)
+if hasil_percakapan:
+
+    return hasil_percakapan
 
 # ==============================
 # Cari Kosakata
